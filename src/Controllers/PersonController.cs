3f4f19e514f362a8b327d3c276a5254c97182b1d
tmzplaydev <tmzplaydev@gmail.com>
2022-09-01 21:40:26 -0300

    Some refactors and improving legibillity

--- src/Controllers/PersonController.cs
+++ src/Controllers/PersonController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using src.Models;
 using src.Persistence;
+using System.Net;
 
 namespace src.Controllers;
 
@@ -16,35 +17,85 @@ public class PersonController : ControllerBase
     private Context _context { get; set; }
 
     [HttpGet]
-    public List<Person> ListAll()
+    public ActionResult<List<Person>> ListAll()
     {
-        // Person Person = new Person("Evandro", 35, "12345678912");
-        // Contract newContract = new Contract("abc123", 50.98);
-        // Person.Contracts.Add(newContract);
-        return _context.Persons.Include(p => p.Contracts).ToList();
+        var result = _context.Persons.Include(p => p.Contracts).ToList();
+
+        if (!result.Any()) return NoContent();
+
+        return Ok(result);
     }
 
     [HttpPost]
-    public Person Insert([FromBody] Person person)
+    public ActionResult<Person> Insert([FromBody] Person person)
     {
-        _context.Persons.Add(person);
-        _context.SaveChanges();
-        return person;
+        try
+        {
+            _context.Persons.Add(person);
+            _context.SaveChanges();
+        }
+        catch (System.Exception)
+        {
+            return BadRequest(new
+            {
+                msg = "Não foi possível processar a solicitação"
+            });
+        }
+
+        return Created("Criado com sucesso!", person);
     }
 
     [HttpPut("{id}")]
-    public string Update([FromRoute] int id, [FromBody] Person person)
+    public ActionResult<Object> Update([FromRoute] int id, [FromBody] Person person)
     {
-        _context.Persons.Update(person);
-        _context.SaveChanges();
-        return "Dados do ID: " + id + " atualizado";
+        var result = _context.Persons.SingleOrDefault(e => e.Id == id);
+        if (result is null)
+        {
+            return NotFound(new
+            {
+                msg = $"Não foi possível encontrar o registro {id} no sistema.",
+                status = HttpStatusCode.NotFound
+            });
+        }
+
+        try
+        {
+            _context.Persons.Update(person);
+            _context.SaveChanges();
+
+        }
+        catch (System.Exception)
+        {
+            return BadRequest(new
+            {
+                msg = "Não foi possível realizar essa operação.",
+                status = HttpStatusCode.BadRequest
+            });
+        }
+        return new
+        {
+            msg = $"Registro {id} atualizado com sucesso!",
+            status = HttpStatusCode.OK
+        };
     }
 
     [HttpDelete("{id}")]
-    public string Delete([FromRoute] int id, [FromBody] Person person)
+    public ActionResult<Object> Delete([FromRoute] int id, [FromBody] Person person)
     {
-        _context.Persons.Remove(person);
-        return "Pessoa com ID: " + id + " deletado com sucesso!";
+        var result = _context.Persons.SingleOrDefault(e => e.Id == id);
+
+        if (result is null) return BadRequest(new
+        {
+            msg = "Conteúdo inexistente. Solicitação inválida",
+            status = HttpStatusCode.Forbidden
+        });
+        _context.Persons.Remove(result);
+        _context.SaveChanges();
+        return Ok(new
+        {
+            msg = "Registro deletado com sucesso!",
+            status = HttpStatusCode.OK
+        });
     }
 
 }
